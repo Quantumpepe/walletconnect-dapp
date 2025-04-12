c2cd67a2410a7f0f47d7920dafa05c180c427ef8
Quantumpepe <xquantumpepexgmail.com>
2025-04-12 16:18:46 +0200

    Final deploy with correct base path

--- assets/index-DHs8wQTY.js
+++ assets/index-DHs8wQTY.js
@@ -1,4 +1,4 @@
-import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-DJlmu_ld.js";import{h as fo}from"./index-DYGs97aK.js";/**
+import{M as rt,T as Oe,R as j,C as P,a as me,O as K,b as ne,E as q,c as mo}from"./index-BHrAH15R.js";import{h as fo}from"./index-tgbtgpFV.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
